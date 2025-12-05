3139602a0340f798c283194107ee0b638d300323
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-12-04 22:41:09 -0500

    Add Repo Name

--- github.js
+++ github.js
@@ -1,10 +1,7 @@
-// This file contains the GitHub repository configuration.
-// It is intended to be environment-specific and should not be committed to Git.
 const githubConfig = {
-  repoName: "Case-Study-416"
+  repoName: "https://github.com/arslvn93/Case-Study-960"
 };
 
-// Make it available to the browser window
 if (typeof window !== 'undefined') {
   window.githubConfig = githubConfig;
 }
